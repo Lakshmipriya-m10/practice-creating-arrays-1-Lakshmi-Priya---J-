a1a86ca79864d7b7f59cc2e1efee1d6ef9830db6
lakshmi priya <lakshmipriyaark@gmail.com>
2025-10-08 20:01:07 -0500

    creating array practice 1 file added

---
+++ createarray.js
@@ -0,0 +1,6 @@
+let groItems = ["salt", "pepper", "eggs", "milk", "bread"];
+let prices = [2.99, 1, 4.95]
+console.log(groItems);
+console.log(prices);
+console.log(groItems[0], groItems[1], groItems[4]);
+console.log(prices[1], prices[2]);
